4d1aa06c3377b616d4510232d68fae6e0b53c305
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-03-07 11:56:20 +0000

    Got the scripts up and running

--- ssv2/customerClient_done.ts
+++ ssv2/customerClient_done.ts
@@ -7,10 +7,14 @@
  * @NApiVersion 2.1
  */
 
+import currentRecord = require('N/currentRecord');
+import url   = require('N/url');
+
 export function pageInit() { // This entry point is not actually implemented
 
 }
 
-export function printInvoicesBtn() {
-  alert('Coming soon');
+export async function printInvoicesBtn() {
+  const rec = await currentRecord.get.promise();
+  window.location.href = url.resolveScript({ scriptId: 'customscript_print_invoices_sl_done', deploymentId: 'customdeploy1', params: { customer: rec.id } });
 }
--- ssv2/printInvoicesSuitelet_done.ts
+++ ssv2/printInvoicesSuitelet_done.ts
@@ -9,7 +9,42 @@
  */
 
 import {EntryPoints} from "N/types";
+import log = require('N/log');
+import query = require('N/query');
+import render = require('N/render');
+import runtime = require('N/runtime');
+import url = require('N/url');
+import xml = require('N/xml');
 
 export function onRequest(context: EntryPoints.Suitelet.onRequestContext) {
-
+  const customerId = context.request.parameters['customer'];
+  log.debug(context.request.method, `Looking up invoices for customer ${customerId}.`);
+  // Step 1: Find invoices.  Feel free to add additional filters here, such as open invoices only.
+  const invoiceResults: { id: number }[] = query.runSuiteQL({
+    query: `SELECT id FROM transaction WHERE entity = ${customerId} AND type = 'CustInvc'`
+  }).asMappedResults() as any;
+  log.debug('GET', `Customer ${customerId} invoices (${invoiceResults.length}): ${JSON.stringify(invoiceResults)}`);
+  // Step 2: Print individual invoices
+  const pdfIds: number[] = [];
+  for (const invoice of invoiceResults) {
+    const invoicePDF = render.transaction({ entityId: invoice.id });
+    invoicePDF.folder = 11609;
+    invoicePDF.isOnline = true; // Important!
+    const pdfId = invoicePDF.save(); // Each invoice costs 30 points to create
+    log.debug('GET', `Created PDF ${pdfId} for invoice ${invoice.id}, usage remaining: ${runtime.getCurrentScript().getRemainingUsage()}`);
+    pdfIds.push(pdfId);
+  }
+  // Step 3: Generated a merged PDF containing all the individual invoice PDFs
+  const domain = url.resolveDomain({ hostType: url.HostType.APPLICATION });
+  let xmlString = '<?xml version="1.0" encoding="UTF-8" ?>';
+  xmlString    += '<!DOCTYPE pdf PUBLIC "-//big.faceless.org//report" "report-1.1.dtd">';
+  xmlString    += '<pdfset>';
+  const fileResults: { url: string }[] = query.runSuiteQL({ query: `SELECT url FROM file WHERE id IN (${pdfIds.join(',')})` }).asMappedResults() as any;
+  for (const result of fileResults) {
+    let fileUrl = `https://${domain}` + result.url;
+    fileUrl = xml.escape({ xmlText: fileUrl });
+    xmlString += `<pdf src="${fileUrl}" />`;
+  }
+  xmlString += '</pdfset>';
+  context.response.renderPdf({ xmlString });
 }
